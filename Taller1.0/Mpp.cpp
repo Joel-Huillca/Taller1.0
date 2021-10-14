5ac2bb8cf7268015d3ba9e3794859aed5e43de53
Matias Nuñez Salomon <n_matias@hotmail.es>
2021-10-14 19:47:09 -0300

    clase mpp

--- Taller1.0/Mpp.cpp
+++ Taller1.0/Mpp.cpp
@@ -1,1 +1,46 @@
 #include "Mpp.h"
+
+Mpp::Mpp()
+{
+}
+
+Mpp::Mpp(int ancho, int largo)
+{
+	this->ancho = ancho;
+	this->largo = largo;
+	this->ACOL = new Nodo[ancho + 1];
+	this->AROW = new Nodo[largo + 1];
+
+}
+
+int Mpp::getAncho()
+{
+	return this->ancho;
+}
+
+int Mpp::getLargo()
+{
+	return this->largo;
+}
+
+void Mpp::setAncho(int ancho)
+{
+	this->ancho = ancho;
+}
+
+void Mpp::setLargo(int largo)
+{
+	this->largo = largo;
+}
+
+
+
+
+
+
+
+
+
+Mpp::~Mpp()
+{
+}
--- Taller1.0/Mpp.h
+++ Taller1.0/Mpp.h
@@ -1,12 +1,34 @@
 #pragma once
 #include <iostream>
 #include <string>
+#include "Nodo.h"
 
 class Mpp
 {
 private:
-	int filas;
-	int columnas;
+	int ancho; // numero de columnas
+	int largo; // numero de filas
+	Nodo* ACOL;
+	Nodo* AROW;
+public:
+	//constructores
+	Mpp();
+	Mpp(int ancho, int largo);
+
+	//gets y sets
+	int getAncho();
+	int getLargo();
+	void setAncho(int ancho);
+	void setLargo(int largo);
+
+	//metodos
+
+	//destructor
+	~Mpp();
+
+
+
+
 	
 };
 
