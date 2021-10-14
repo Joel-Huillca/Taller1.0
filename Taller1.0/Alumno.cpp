cb4644fb3bbe21fc246c42d3f7a9028acc82b342
Matias Nuñez Salomon <n_matias@hotmail.es>
2021-10-14 17:40:22 -0300

    clase alumno

--- Taller1.0/Alumno.cpp
+++ Taller1.0/Alumno.cpp
@@ -1,1 +1,72 @@
 #include "Alumno.h"
+
+Alumno::Alumno()
+{
+}
+
+Alumno::Alumno(string nombre, string rut, float notaUno, float notaDos, float notaTres, bool apruebaTaller, int paralelo, int numLista)
+{
+	this->nombre = nombre;
+	this->rut = rut;
+	this->notaUno = notaUno;
+	this->notaDos = notaDos;
+	this->notaTres = notaTres;
+	this->apruebaTaller = apruebaTaller;
+	this->paralelo = paralelo;
+	this->numLista = numLista;
+}
+
+string Alumno::getNombre()
+{
+	return nombre;
+}
+
+string Alumno::getRut()
+{
+	return rut;
+}
+
+float Alumno::getNotaUno()
+{
+	return notaUno;
+}
+
+float Alumno::getNotaDos()
+{
+	return notaDos;
+}
+
+float Alumno::getNotaTres()
+{
+	return notaTres;
+}
+
+bool Alumno::getApruebaTaller()
+{
+	return apruebaTaller;
+}
+
+int Alumno::getParalelo()
+{
+	return paralelo;
+}
+
+int Alumno::getNumLista()
+{
+	return numLista;
+}
+
+float Alumno::setNotaUno(float notaPruebaRec)
+{
+	return this->notaUno = notaPruebaRec;
+}
+
+float Alumno::setNotaDos(float notaPruebaRec)
+{
+	return this->notaDos = notaPruebaRec;
+}
+
+float Alumno::setNotaTres(float notaPruebaRec)
+{
+	return this->notaTres = notaPruebaRec;
+}
--- Taller1.0/Alumno.h
+++ Taller1.0/Alumno.h
@@ -1,8 +1,37 @@
 #pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+
 class Alumno
 {
-	
+private:
+	string nombre;
+	string rut;
+	float notaUno;
+	float notaDos;
+	float notaTres;
+	bool apruebaTaller;
+	int paralelo;
+	int numLista;
+public:
+	//constructores
+	Alumno();
+	Alumno(string nombre, string rut, float notaUno, float notaDos, float notaTres, bool apruebaTaller, int paralelo, int numLista);
 
+	//gets y sets
+	string getNombre();
+	string getRut();
+	float getNotaUno();
+	float getNotaDos();
+	float getNotaTres();
+	bool getApruebaTaller();
+	int getParalelo();
+	int getNumLista();
+
+	float setNotaUno(float notaPruebaRec);
+	float setNotaDos(float notaPruebaRec);
+	float setNotaTres(float notaPruebaRec);
 
 };
 
