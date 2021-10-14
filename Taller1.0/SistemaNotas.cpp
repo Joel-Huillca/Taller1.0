7cef269cdfb9de4e7ed818d51838e6a700cc1d7d
Yo o <joel.huillca@alumnos.ucn.cl>
2021-10-14 19:23:41 -0300

    menu1.0

--- Taller1.0/SistemaNotas.cpp
+++ Taller1.0/SistemaNotas.cpp
@@ -16,7 +16,7 @@ void SistemaNotas::menuIniciarSesion()
 	string nombre;
 	cout << "Nombre: ";
 	getline(cin, nombre);
-	//Buscar si nombre es igual la Matriz de nombres
+	//Buscar si nombre existe dentro de la Matriz[][]
 	//If (existe){}else {null o diferente}
 
 	string codigo;
@@ -26,7 +26,6 @@ void SistemaNotas::menuIniciarSesion()
 		cout << "Ya podemos iniciar...\n" << endl;
 	}
 
-
 }
 
 void SistemaNotas::menuPrincipal()
@@ -34,7 +33,18 @@ void SistemaNotas::menuPrincipal()
 	
 	cout << "Bienvenido\n" << endl;
 	menuIniciarSesion();
-	
+	bool salir = false;
+	while (salir == false) {
+		cout << "[1] Cargar archivos\n[2] Registrar Notas\n[3] Estadisticas\n[4] Elminar Estudiantes R.\n[5] Salir"<<endl;
+		string texto1 = "> Elija una opcion:";
+		int opcion1 = recibirDato(texto1);
+
+
+
+		salir = true;
+		
+
+	}
 
 }
 
