d773be199b9e3ed9605e6b94a3fbb96b3062be4a
Matias Nuñez Salomon <n_matias@hotmail.es>
2021-10-15 15:01:16 -0300

    SS

--- Taller1.0/SistemaNotas.cpp
+++ Taller1.0/SistemaNotas.cpp
@@ -4,7 +4,6 @@
 using namespace std;
 SistemaNotas::SistemaNotas()
 {
-	leerArchivo();
 }
 
 SistemaNotas::~SistemaNotas()
@@ -22,61 +21,101 @@ void SistemaNotas::menuIniciarSesion()
 	string codigo;
 	cout<<  "Codigo:";
 	getline(cin,codigo);
-	if (codigo._Equal("holi")) {
+	if (codigo._Equal("123")) {
 		cout << "Ya podemos iniciar...\n" << endl;
 	}
+	else
+		cout << "**Contraseña incorrecta**\n\n";
 }
 
 void SistemaNotas::menuPrincipal()
 {
-	
+	bool lecturaArch = false;
+
+
 	cout << "_____.:Bienvenido:._____\n" << endl;
 	menuIniciarSesion();
 	bool salir = false;
 	while (salir == false) {
-		cout << "[1] Cargar archivos\n[2] Registrar Notas\n[3] Estadisticas\n[4] Elminar Estudiantes R.\n[5] Salir"<<endl;
+		cout << "[1] Cargar archivos\n[2] Registrar Notas\n[3] Estadisticas\n[4] Elminar Estudiantes R.\n[5] Salir\n";
 		string texto1 = "> Elija una opcion:";
 		int opcion1 = recibirDato(texto1);
+<<<<<<< HEAD
 		switch (opcion1)
 		{
 		case 1:
 		{
 			leerArchivo();
+=======
+		switch (opcion1) {
+		
+		case 1: {
+
+			if (lecturaArch == false)
+			{
+				leerArchivo();
+				lecturaArch = true;
+			}
+			else {
+				cout << "Ya tiene cargada la informacion al sistema \n\n";
+			}
+>>>>>>> 84311775a049a94c415e0805343b4fc0a8284e54
 		}
 		break;
+
 		case 2:
-		{}
+		{
+			if (lecturaArch == true)
+			{
+				cout << "2222222222222\n";
+			}
+			else
+			{
+				cout << "Favor de '[1] Cargar archivos'\n";
+			}
+
+		}
 		break;
 		case 3:
 		{
-			cout << "          |ESTADISTICAS|"<<endl;
-			cout << "[1] Cantidad de estudiantes que esten reprobando el Taller por paralelo"<<endl;
-			cout << "[2] % Total de estudiantes que estan reprobando el Curso" << endl;
-			cout << "[3] Promedio final de catedras por cada estudiante " << endl;
-			string texto2 = "> Elija una opcion:";
-			int opcion2 = recibirDato(texto2);
-			switch (opcion2)
+			if (lecturaArch == true)
 			{
-			case 1:
-			{cout << "11111111111111";}
-			break;
-			case 2:
-			{cout << "222222222222"; }
-			break; 
-			case 3:
-			{cout << "333333333333"; }
-			break;
-
-			default:
-				cout << "La opcion ingresada no es valida, intente de nuevo...\n" << endl;
+				cout << "          |ESTADISTICAS|" << endl;
+				cout << "[1] Cantidad de estudiantes que esten reprobando el Taller por paralelo" << endl;
+				cout << "[2] % Total de estudiantes que estan reprobando el Curso" << endl;
+				cout << "[3] Promedio final de catedras por cada estudiante " << endl;
+				string texto2 = "> Elija una opcion:";
+				int opcion2 = recibirDato(texto2);
+				switch (opcion2)
+				{
+				case 1:
+				{cout << "11111111111111"; }
 				break;
-			}//Fin del menu_2
+				case 2:
+				{cout << "222222222222"; }
+				break;
+				case 3:
+				{cout << "333333333333"; }
+				break;
+
+				default:
+					cout << "La opcion ingresada no es valida, intente de nuevo...\n" << endl;
+					break;
+				}//Fin del menu_2
+			}
+			else
+			{
+				cout << "Favor de '[1] Cargar archivos'\n";
+			}
 
 		}
 		break;
+
+
 		case 4:
 		{}
 		break;
+
 		case 5:
 		{
 			salir = true;
@@ -155,7 +194,7 @@ void SistemaNotas::leerArchivo()
 		float notaTres;
 
 		string apruebaTallerAux;
-		bool apruebaTaller;      //--
+		bool apruebaTaller;      
 
 		string paraleloAux;
 		int paralelo;
@@ -206,16 +245,5 @@ void SistemaNotas::leerArchivo()
 	leerArch = true;
 	archivo.close();
 	//imprimir
-	for (int i = 1; i <= largo; i++) {
-		string fila;
-		Nodo* aux = AROW[i].getLeft();
-		int espacios = ancho - aux->getColumna();
-		while (true)
-		{
-			for (int j = 0; j < espacios; j++) {
-				fila = "0" + fila;
-			}
-			if(aux->getColumna() == 0 )
-		}
-	}
+
 }
